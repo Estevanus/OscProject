baecd0274c20497cb5c3c573fb9c6e38d8d5da11
Estevanus <oscartoreh@gmail.com>
2018-11-09 16:07:45 +0800

    Add files via upload

--- Tambahan/Libs/osclib/osclib/osclib.h
+++ Tambahan/Libs/osclib/osclib/osclib.h
@@ -12,6 +12,14 @@ extern "C" OSCLIB_API int kali(int a, int b);
 //int kali(int a, int b);
 //extern "C" OSCLIB_API double kali2(double x, double y);
 extern "C" OSCLIB_API float kali2(float a, float b);
+extern "C" OSCLIB_API double *kali4(double a, double b);
+extern "C" OSCLIB_API double kali5(double a, double b);
+extern "C" OSCLIB_API int loopAll(int max);
 extern "C" OSCLIB_API PyObject kali3(float a, PyObject b);
+
+
+// ----------------------------- My Lib Data -----------------------------
+extern "C" OSCLIB_API double getMaxSpeed(double force, double airDensity, double drag, double frontalArea);
+// -----------------------------------------------------------------------
 
 
